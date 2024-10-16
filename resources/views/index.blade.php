fe78ccacc52030005a4630eade24bc4649ada895
shefiazzahra27 <541231198@student.smktelkom-pwt.sch.id>
2024-10-16 14:36:43 +0700

    add index and image

---
+++ resources/views/index.blade.php
@@ -0,0 +1,137 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"content="width=device-width, intial-scale=1.0">
+    <title>About Lee Heeseung</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <nav>
+       <div class="wrapper">
+           <div class="logo"><a href=' '>EN-</a></div>
+           <div class="menu"></div>
+               <ul>
+                  <li><a href="home">Home</a></li>
+                  <li><a href="#Fact">Fact</a></li>
+                  <li><a href="#Pre-debut">Pre-debut</a></li>
+                  <li><a href="#favorite">Favorite</a></li>
+                  <li><a href="#Hobies&skills">Hobies&skills</a></li>
+                  <li><a href=""class="tbl-biru">join engene!</a></li>
+               </ul>
+
+       </div>
+    </nav>
+    <div class="wrapper">
+        <section id="home">
+            <img src="assets/image/1.jpeg" width="500" height="400"/>
+            <div class="kolom">
+                <p class="deskripsi"> Mari join ngestan enhypen!</p>
+                <h2> Tetap solid dan tetap kuat melihat heeseung lee</h2>
+                <p> Jangan ragu, jangan bimbang, mari ngestan heesung. dijamin anda akan meleyottt</p>
+                <p><a href="" class="tbl-pink">ayo ngestan lebih dalam lagi</a></p>
+            </div>
+        </section> 
+
+        <!-- untuk courses -->
+        <section id="courses">
+            <div class="kolom">
+                <p class="deskripsi">Litte Info</p>
+                <p>- He is the eldest member of the group, being a ‘01 liner.He also has an English name and people know it is 'Ethan Lee'
+                     but he tells his fans that his English name is 'Evan'. During the Enhypen debut showcase interview, he said that a character most similar to him is Bambi.
+                    He has cited SHINee as his role model when he was in middle school. Other than that, for his applicant profile for I-land as well as the 2020 December issue for Seventeen Magazine, he referred to his dad as his role model and hero.                   
+                    If he were to become the main character of a movie, he would choose being a male lead of a Japanese romance animation, specifically Howl from Howl’s Moving Castle.                   
+                    He once stated that a mic is something he cannot live without.</p>
+                <p><a href="" class="tbl-biru">ngestan lebih dalam lagi</a></p>
+            </div>
+        </section>
+
+        <!-- untuk tutors -->
+        <section id="tutors">
+            <div class="tengah">
+                <div class="kolom">
+                    <p class="deskripsi">His partner</p>
+                    <h2>Members</h2>
+                    <p>One, Two, Connect! Hello, we are enhypen!</p>
+                </div>
+
+                <div class="tutor-list">
+                    <div class="kartu-tutor">
+                        <img src="assets/image/2.jpeg"/>
+                        <p>Sim Jake</p>
+                    </div>
+                    <div class="kartu-tutor">
+                        <img src="assets/image/3.jpeg"/>
+                        <p>Jay Park</p>
+                    </div>
+                    <div class="kartu-tutor">
+                        <img src="assets/image/4.jpeg"/>
+                        <p>Park Sunghoon</p>
+                    </div>
+                    <div class="kartu-tutor">
+                        <img src="assets/image/5.jpeg"/>
+                        <p>Kim Sunoo</p>
+                    </div>
+                    <div class="kartu-tutor">
+                        <img src="assets/image/6.jpeg"/>
+                        <p>Yang Jungwon</p>
+                    </div>
+                    <div class="kartu-tutor">
+                        <img src="assets/image/7.jpeg"/>
+                        <p>Nishimura Riki</p>
+                </div>
+            </div>
+        </section>
+
+        <!-- untuk partners -->
+        <section id="partners">
+            <div class="tengah">
+                <div class="kolom">
+                    <p class="deskripsi">(Our Top Fans)</p>
+                    <h2>ENGENE</h2>
+                </div>
+
+                <div class="partner-list">
+                    <div class="kartu-partner">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <div id="contact">
+        <div class="wrapper">
+            <div class="footer">
+                <div class="footer-section">
+                    <h3>Instagram</h3>
+                    <p>@enhypen</p>
+                </div>
+                <div class="footer-section">
+                    <h3>Twitter</h3>
+                    <p>@Enhypen_members
+                       @Enhypen official
+                       @Enhypen_JP
+                    </p>
+                </div>
+                <div class="footer-section">
+                    <h3>Website ofc</h3>
+                    <p>https://beliftlab.com/artist/profile/ENHYPEN</p>
+                </div>
+                <div class="footer-section">
+                    <h3>Social</h3>
+                    <p><b>YouTube: </b>ENHYPEN</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="copyright">
+        <div class="wrapper">
+            &copy; 2023. <b>Heeseungie.</b> All Rights Reserved.
+        </div>
+    </div>
+
+</body>
+</html>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('Tentang Que');
 });
